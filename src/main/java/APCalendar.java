06d3f5fe80c02ab13b641abbb76307dcb8bcb9ba
derricckk11 <132044722+derricckk11@users.noreply.github.com>
2025-04-30 11:42:09 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -22,11 +22,9 @@ public class APCalendar
 
         count ++;
       }
-
-      return count;
-      
     }
-
+    return count;
+      
   }
   
   /** Returns the value representing the day of the week for the first day of year,
